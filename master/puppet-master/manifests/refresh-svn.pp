87b2a390b0bd46b9c4916ffe4568d0ebd056b2b1
Bibhuti93 <bibhuti.mymail@gmail.com>
2018-04-21 13:43:44 +0530

    Added ":" in line vcsrepo { '/etc/puppetlabs/puppet/modules/':

--- master/puppet-master/manifests/refresh-svn.pp
+++ master/puppet-master/manifests/refresh-svn.pp
@@ -1,5 +1,5 @@
 #Added a comment
-vcsrepo { '/etc/puppetlabs/puppet/modules/'
+vcsrepo { '/etc/puppetlabs/puppet/modules/':
 			ensure => latest,
 			provider => git,
 			source => 'git://github.com/Bibhuti93/puppet-auto.git',
