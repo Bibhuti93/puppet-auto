71f6011606c7c5c73efedfc3908f0b39f1d81664
Bibhuti93 <bibhuti.mymail@gmail.com>
2018-04-21 08:41:30 +0530

    Update and rename master-init.pp to master-post-startup.pp

--- master-post-startup.pp
+++ master-post-startup.pp
@@ -3,7 +3,6 @@ file { '/etc/puppetlabs/puppet/autosign.conf':
        ensure => file,
        owner => 'root',
        group => 'root',
-       content => 'echo hostname',
       }
 
 #Install GIT package
