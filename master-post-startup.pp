c366551c690e377556bcf2762cd4f783d7ad5858
Bibhuti93 <bibhuti.mymail@gmail.com>
2018-04-21 08:46:28 +0530

    Update master-post-startup.pp

--- master-post-startup.pp
+++ master-post-startup.pp
@@ -1,9 +1,19 @@
 #Enable autosign
-file { '/etc/puppetlabs/puppet/autosign.conf':
-       ensure => file,
-       owner => 'root',
-       group => 'root',
-      }
+file { '/opt/puppet/bin/gce_autosigner.rb':
+    ensure => file,
+    owner  => 'pe-puppet',
+    group  => 'pe-puppet',
+    mode   => '0750',
+    source => 'puppet:///modules/gce_compute/gce_autosigner.rb',
+  }
+  ini_setting { 'autosign':
+    ensure  => present,
+    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    section => 'master',
+    setting => 'autosign',
+    value   => '/opt/puppet/bin/gce_autosigner.rb',
+    require => File['/opt/puppet/bin/gce_autosigner.rb'],
+  }
 
 #Install GIT package
 package { 'git':
